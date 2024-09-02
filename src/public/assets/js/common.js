edfd329abf5a2a601b793268c1180e1c78848e58
김채민 <dktjdej25@gmail.com>
2024-09-02 10:46:11 +0900

    새 기능: 헤더 추가
    1.타이머 페이지 이동 버튼
    2. 관리자 페이지 이동 버튼
    3. 테마 변경 기능

--- src/public/assets/js/common.js
+++ src/public/assets/js/common.js
@@ -23,3 +23,36 @@ async function timerUpdate(event) {
 
     alert('Timer updated');
 }
+
+function themeToggle() {
+    // 토글 버튼
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    // 토글 아이콘
+    const themeIcon = document.getElementById('theme-icon');
+    // html 태그
+    const htmlElement = document.documentElement;
+
+    // 기본 테마 세팅
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        htmlElement.setAttribute('data-theme', currentTheme);
+        themeIcon.textContent = currentTheme === 'dark' ? '🌙' : '☀️';
+    }
+
+    // 테마 변경
+    themeToggleBtn.addEventListener('click', () => {
+         //현재 테마
+        const currentTheme = htmlElement.getAttribute('data-theme');
+        // 변경할 테마
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        htmlElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+
+        // 아이콘 변경
+        themeIcon.textContent = newTheme === 'dark' ? '🌙' : '☀️';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    themeToggle();
+});
